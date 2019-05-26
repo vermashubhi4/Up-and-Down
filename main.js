7c846af8ff3efb488b20fda70cbe4a1343ff1490
Shubhi Verma <vermashubhi4@gmail.com>
2019-05-27 00:05:48 +0530

    changes:(

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ var dx=15,dy=0;
 var left=37,up=38,right=39,down=40,space=32;
 var key;
 var timer;
-var speed=50;
+var speed=100;
 
 var foodx,foody,foodth=15;
 var snakex=[x];
@@ -34,16 +34,20 @@ foody=Math.floor(Math.random()*(580));
 
 ctx.fillStyle="#D32802";
 ctx.fillRect(foodx,foody,foodth,foodth);
-var lastKey=left;
+var lastKey=[right,right];
 
 function isin(x1,y1,x2,y2)
 {
+  console.log("isin x:",snakex);
+  console.log("isin y:",snakey);
   if(x2>=x1 && y2>=y1)
   {
     if(x2<=x1+th && y2<=y1+th)
     {
       clearInterval(timer);
+
       console.log("Game Over");
+      die();
     }
   }
 
@@ -51,92 +55,87 @@ function isin(x1,y1,x2,y2)
 
 window.onkeydown=function whichKey(event){
  key = event.keyCode;
-
-  if(key==left && lastKey!=right)
+if(timer!=-1){
+  if(key==left && lastKey[0]!=right)
   {
-    // if(lastKey==space)
-    // {
-    //   timer=setInterval(move,speed);
-    // }
-    lastKey=key;
+    lastKey[1]=lastKey[0];
+    lastKey[0]=key;
     dx=-15;
     dy=0;
+    clearInterval(timer);
+    move();
+    timer=setInterval(move,speed);
   }
-   if(key==up && lastKey!=down)
+   if(key==up && lastKey[0]!=down)
    {
-     // if(lastKey==space)
-     // {
-     //   timer=setInterval(move,speed);
-     // }
-     lastKey=key;
+     lastKey[1]=lastKey[0];
+     lastKey[0]=key;
      dy=-15;
      dx=0;
+     clearInterval(timer);
+     move();
+     timer=setInterval(move,speed);
 
    }
-    if(key==right && lastKey!=left)
+    if(key==right && lastKey[0]!=left)
     {
-      // if(lastKey==space)
-      // {
-      //   timer=setInterval(move,speed);
-      // }
-      lastKey=key;
+      lastKey[1]=lastKey[0];
+      lastKey[0]=key;
       dx=15;
       dy=0;
 
     }
-     if(key==down && lastKey!=up)
+     if(key==down && lastKey[0]!=up)
      {
-       // if(lastKey==space)
-       // {
-       //   timer=setInterval(move,speed);
-       // }
-        lastKey=key;
+       lastKey[1]=lastKey[0];
+       lastKey[0]=key;
        dy=15;
        dx=0;
+       clearInterval(timer);
+       move();
+       timer=setInterval(move,speed);
 
      }
      if(key==space)
      {
-
-       if(timer==0)
+       console.log("space\n");
+      if(timer==0)
        {
+         console.log("spacex");
+         timer=setInterval(move,speed);
+         clearInterval(timer);
+         move();
          timer=setInterval(move,speed);
        }
        else {
+          console.log("spacey");
          clearInterval(timer);
          timer=0;
        }
-       lastKey=key;
-
+       lastKey[1]=lastKey[0];
+       lastKey[0]=key;
      }
+   }
 }
 
 function move(){
   var i;
-
-  //count=count+1;
-  //console.log(x,y,key);
-//  if(count!=1)
    ctx.clearRect(snakex[snakex.length-1],snakey[snakey.length-1],th,th);
-
+   console.log("move x:",snakex);
+   console.log("move y: ",snakey);
    snakex[0]=snakex[0]+dx;
    snakey[0]=snakey[0]+dy;
-  if(snakex[0]>canvas.width)
+  if(snakex[0]>canvas.width-15)
   {
    snakex[0]=0;
   }
-  if(snakey[0]>canvas.height)
+  if(snakey[0]>canvas.height-15)
   {
      snakey[0]=0;
   }
   if(snakex[0]<0 )
   {
-    // console.log("x ax",snakex);
-    // console.log("x ay",snakey);
-    //ctx.clearRect(snakex[snakex.length-1],snakey[snakey.length-1],th,th);
-    snakex[0]=canvas.width;
-    // console.log("x bx",snakex);
-    // console.log("x by",snakey);
+    snakex[0]=canvas.width-15;
   }
   if(snakex[snakex.length-1]<0)
   {
@@ -144,27 +143,21 @@ function move(){
   }
   if(snakey[0]<0)
   {
-    // console.log("y ax",snakex);
-    // console.log("y ay",snakey);
-    //ctx.clearRect(snakex[snakex.length-1],snakey[snakey.length-1],th,th);
-     snakey[0]=canvas.height;
-
-     // console.log("y bx",snakex);
-     // console.log("y by",snakey);
+     snakey[0]=canvas.height-15;
   }
-
+//Setting color of snake to green
   ctx.fillStyle="#148F77";
   ctx.fillRect(snakex[0],snakey[0],th,th);
-
-if(lastKey==up || lastKey==down || lastKey==space)
+//Setting color of snake's stipes
+if(lastKey[0]==up || lastKey[0]==down || lastKey[0]==space || lastKey[1]==up || lastKey[1]==down)
   {
   ctx.fillStyle="#282c34";
-  ctx.fillRect(snakex[0],snakey[0],5,5);
+  ctx.fillRect(snakex[0],snakey[0],th,2);
   }
-  if(lastKey==right || lastKey==left || lastKey==space)
+  if(lastKey[0]==right || lastKey[0]==left || lastKey[0]==space ||  lastKey[1]==left || lastKey[1]==right)
   {
       ctx.fillStyle="#282c34";
-      ctx.fillRect(snakex[0],snakey[0],5,5);
+      ctx.fillRect(snakex[0],snakey[0],2,th);
   }
 if((snakex[0]>=foodx && snakex[0]<=foodx+foodth && snakey[0]>=foody && snakey[0]<=foody+foodth)
   ||(snakex[0]+th>=foodx && snakey[0]+th>=foody && snakex[0]+th<=foodx+foodth && snakey[0]<=foody+foodth)
@@ -173,8 +166,8 @@ if((snakex[0]>=foodx && snakex[0]<=foodx+foodth && snakey[0]>=foody && snakey[0]
   )
  {
      eat();
-     console.log(snakex);
-     console.log(snakey)
+     // console.log(snakex);
+     // console.log(snakey)
  }
 
  if(snakex.length>=4)
@@ -184,76 +177,67 @@ if((snakex[0]>=foodx && snakex[0]<=foodx+foodth && snakey[0]>=foody && snakey[0]
      isin(snakex[i],snakey[i],snakex[0],snakey[0]);
    }
  }
- // snakex[0]=snakex[0]+dx;
- // snakey[0]=snakey[0]+dy;
+
  ctx.fillStyle="#148F77";
  ctx.fillRect(snakex[0],snakey[0],th,th);
  for(i=snakex.length-1;i>0;i--)
  {
-   // console.log(snakex);
-   // console.log(snakey)
-   console.log(i);
      snakex[i]=snakex[i-1];
      snakey[i]=snakey[i-1];
+
    ctx.fillStyle="#148F77";
    ctx.fillRect(snakex[i],snakey[i],th,th);
-   if(lastKey==up || lastKey==down || lastKey==space)
+   if(lastKey[0]==up || lastKey[0]==down || lastKey[0]==space || lastKey[1]==up || lastKey[1]==down )
      {
      ctx.fillStyle="#282c34";
-     ctx.fillRect(snakex[i],snakey[i],5,5);
+     ctx.fillRect(snakex[i],snakey[i],th,2);
      }
-     if(lastKey==right || lastKey==left || lastKey==space)
+     if(lastKey[0]==right || lastKey[0]==left || lastKey[0]==space ||lastKey[1]==left || lastKey[1]==right)
      {
          ctx.fillStyle="#282c34";
-         ctx.fillRect(snakex[i],snakey[i],5,5);
+         ctx.fillRect(snakex[i],snakey[i],2,th);
      }
-      console.log(i);
+
  }
 
-
+if(snakex.length==2){eat();}
 }
-//var c=0;
+
 function eat()
 {
   count=count+100;
   score.innerHTML=count;
-  console.log("eat");
+//  console.log("eat");
 
-//  console.log(foodx,foody,foodx+foodth,foody+foodth);
-//  console.log(x,y,x+th,y+th);
   snakex.push(snakex[snakex.length-1]-15);
   snakey.push(snakey[snakey.length-1]);
 
-  console.log(snakex.length);
-  //console.log(snakex);
-  //console.log(snakey);
+  //console.log(snakex.length);
 
-  //console.log(snakex[snakex.length-1]);
-  //console.log(snakex[snakey.length-1]);
-  //index=index+1;
   ctx.clearRect(foodx,foody,foodth,foodth);
   foodx=Math.floor(Math.random()*(730));
   foody=Math.floor(Math.random()*(500));
-  //console.log(foodx,foody);
+
   ctx.fillStyle="#D32802";
   ctx.fillRect(foodx,foody,foodth,foodth);
-  /*ctx.fillStyle="#f13a62";
-  ctx.fillRect(snakex[index],snakey[index],th,th);*/
+
 }
 
-function isin(x1,y1,x2,y2)
-{
-  if(x1<=x2 && x2<=x1+th && y1<=y2 && y2<=y1+th)
-  {
-    die();
-  }
-}
+// function isin(x1,y1,x2,y2)
+// {
+//   if(x1<=x2 && x2<=x1+th && y1<=y2 && y2<=y1+th)
+//   {
+//     die();
+//   }
+// }
 
 function die()
 {
+  console.log("die x: ",snakex);
+  console.log("die y: ",snakey);
   clearInterval(timer);
-  timer=0;
+  timer=-1;
   ctx.fillStyle="Red";
   ctx.font="60px Arial";
-  ctx.fillText("Game Over",camvas.width/2,canvas.height/2);
+  ctx.fillText("Game Over",canvas.width/2,canvas.height/2);
 }
